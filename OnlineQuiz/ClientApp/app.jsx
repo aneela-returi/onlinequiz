0cd2f6d80bb72447748b6c7b3d7cfcb84809e8c8
Aneela-Returi <aneela.returi@gmail.com>
2019-02-03 17:02:04 +1100

    fixed testselection in app.jsx

--- OnlineQuiz/ClientApp/app.jsx
+++ OnlineQuiz/ClientApp/app.jsx
@@ -1,7 +1,7 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Testselection from './TestSelection';
+import TestSelection from './TestSelection';
 import Quiz from './quiz';
 
 const Q = function ({ match }) {
@@ -13,8 +13,8 @@ const Q = function ({ match }) {
 const App = () => (
     <div>
         <Switch>
-            <Route exact path="/" component={Testselection} />
-            <Route exact path="/quiz.html" component={Testselection} />
+            <Route exact path="/" component={TestSelection} />
+            <Route exact path="/quiz.html" component={TestSelection} />
             <Route path='/starttest/:test' component={Q} />
         </Switch>
 
